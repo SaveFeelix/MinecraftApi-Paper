b3be2e002c5eb7993c0f2878cdbcf81b2c575093
SaveFeelix <felixhake@web.de>
2022-08-12 11:28:56 +0200

    ==================== ConfigManager.java ==================== Add method ConfigManager#contains Add method ConfigManager#set ============================================================
    ==================== .gitignore ====================
    Add '.idea' folder
    ====================================================
    
    ==================== pom.xml ====================
    Change version
    =================================================

--- src/main/java/de/savefeelix/minecraftapi/manager/ConfigManager.java
+++ src/main/java/de/savefeelix/minecraftapi/manager/ConfigManager.java
@@ -4,9 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ConfigManager {
+public final class ConfigManager {
     public ConfigManager() throws IllegalAccessException {
         throw new IllegalAccessException();
+    }
+
+    public static @NotNull Boolean contains(@NotNull JavaPlugin plugin, @NotNull String path) {
+        return plugin.getConfig().contains(path);
+    }
+
+    public static void set(@NotNull JavaPlugin plugin, @NotNull String path, @NotNull Object object) {
+        plugin.getConfig().set(path, object);
     }
 
     public static <T> @Nullable T getFromConfig(@NotNull JavaPlugin plugin, @NotNull String path) {
